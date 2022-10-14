9d783bb098b65bbcb4f6f6d905ffd6fcf67f3fbf
Vojtavlas <112420135+Vojtavlas@users.noreply.github.com>
2022-10-14 15:52:15 +0200

    Add files via upload

---
+++ Fake_informations.py
@@ -0,0 +1,13 @@
+
+#By Github:VojtaVlas
+from faker import Faker
+fake = Faker()
+print("all fake informations for you")
+print("")
+print("")
+print(fake.name( ) + ": name")
+print(fake.job( ) + ": job")
+print(fake.company( ) + ": company")
+print(fake.email( ) + ": email")
+print(fake.address( ) + ": address")
+
